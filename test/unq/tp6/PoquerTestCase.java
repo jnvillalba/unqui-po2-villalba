628adbec13c0195321483b57f83979e3f41d094e
joaquin villalba <joaquinvillalba8@gmail.com>
2021-05-26 15:14:21 -0300

    Creo tp6

---
+++ test/unq/tp6/PoquerTestCase.java
@@ -0,0 +1,20 @@
+package unq.tp6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PoquerTestCase {
+
+	@BeforeEach
+	public void setUp(){
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
