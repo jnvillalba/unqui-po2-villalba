f5dad5dc0f8f0a1e192e71448be28ca49b1f5618
joaquin villalba <joaquinvillalba8@gmail.com>
2021-05-30 18:01:51 -0300

    a instancias de Carta en lugar de Strings

---
+++ src/unq/tp6/PokerStatusCarta.java
@@ -0,0 +1,9 @@
+package unq.tp6;
+
+public class PokerStatusCarta {
+	
+	public String verificar(Carta carta1, Carta carta2, Carta carta3, Carta carta4, Carta carta5){
+		return "Poker";
+	}
+
+}
--- test/unq/tp6/CartaTestCase.java
+++ test/unq/tp6/CartaTestCase.java
@@ -1,14 +1,15 @@
 package unq.tp6;
 
 import static org.junit.Assert.assertTrue;
-
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CartaTestCase {
 	
+	private PokerStatusCarta poker;
 	private Carta P2;
 	private Carta C2;
 	private Carta D2;
@@ -23,6 +24,7 @@ public class CartaTestCase {
 	public void setUp() throws Exception{
 		
 		//Set Up
+		poker = mock(PokerStatusCarta.class);
 		P2 = new Carta(2, "P");
 		C2 = new Carta(2, "C");
 		D2 = new Carta(2, "D");
@@ -56,6 +58,56 @@ public class CartaTestCase {
 		//Exercise
 		//C10.poseeMismoPalo(C2)
 		
+	}
+	
+	//Un jugador obtiene póquer si de las cinco cartas recibidas,
+	//cuatro cartas iguales en su valor (ejemplo 4 ases, 4 reyes).
+	//P = picas, C = corazones, D = diamantes, T = tréboles
+	
+	
+	@Test
+	void testConPoker() {
+		
+		//Exercise
+		//poker.verificar(P2, C2, D2, T2, C10);
+		
+		//Verify
+		assertTrue(poker.verificar(P2, C2, D2, T2, C10) == "Poker");
+	}
+	
+	@Test
+	void testSinNada() {
+		//Test Double Configuration
+		when(poker.verificar(P2, C2, P6, P10, C10)).thenReturn("Nada");
+		
+		//Verify
+		assertTrue(poker.verificar(P2, C2, P6, P10, C10) == "Nada");
+	}
+	
+	// Color = las cinco cartas son del mismo color y palo
+	
+	@Test
+	void testColor() {
+		
+		//Test Double Configuration
+		when(poker.verificar(P10, P4, P5, P6, P2)).thenReturn("Color");
+		
+		//Verify
+		//el poker.verificar = Exercise
+		assertTrue(poker.verificar(P10, P4, P5, P6, P2) == "Color");
+	}
+	
+	// Trio = tres cartas poseen el mismo valor
+	
+	@Test
+	void testTrio() {
+		
+		//Test Double Configuration
+		when(poker.verificar(P2, C2, D2, P4, C10)).thenReturn("Trio");
+		
+		//Verify
+		//el poker.verificar = Exercise
+		assertTrue(poker.verificar(P2, C2, D2, P4, C10) == "Trio");
 	}
 	
 	
