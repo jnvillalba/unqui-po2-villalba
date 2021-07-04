fae83169b026caa6a360fe66390976aa2521cf0a
Joaquin Villalba <joaquinvillalba8@gmail.com>
2021-07-04 16:20:18 -0300

    Registro refactoreado

--- src/unq/tp10/Registro/RegistroAutomotor.java
+++ src/unq/tp10/Registro/RegistroAutomotor.java
@@ -6,15 +6,26 @@ public class RegistroAutomotor {
 
 	public Boolean debeRealizarVtv(Vehiculo vehiculo, LocalDate fecha) {
 
-		LocalDate fechaFabricacion = vehiculo.getFechaFabricacion();
-		Boolean esVehiculoPolicial = vehiculo.esVehiculoPolicial();
-		String ciudadRadicacion = vehiculo.ciudadRadicacion();
+		//LocalDate fechaFabricacion = vehiculo.getFechaFabricacion();
+		//Boolean esVehiculoPolicial = vehiculo.esVehiculoPolicial();
+		//String ciudadRadicacion = vehiculo.ciudadRadicacion();
 		
 		// vehiculos policiales no realizan vtv, ya que tienen otro tipo de control
-		// s√≥lo realizan vtv los veh√≠culos con m√°s de 1 anio de antiguedad y radicados
+		// si lo realizan vtv los vehiculos con mas de 1 anio de antiguedad y radicados
 		// en 'Buenos Aires'
-		return (!esVehiculoPolicial && fecha.minusYears(1).isAfter(fechaFabricacion)
-				&& ciudadRadicacion.equalsIgnoreCase("Buenos Aires"));
+		
+		//Long Method
+		return (!vehiculo.esVehiculoPolicial() && this.vehiculoConMasDeUnAÒoDeAntiguedad(vehiculo, fecha)
+				&& this.vehiculoRadicadoEnBsAs(vehiculo) );
 
 	}
+	
+	private boolean vehiculoConMasDeUnAÒoDeAntiguedad(Vehiculo vehiculo, LocalDate fecha) {
+		return fecha.minusYears(1).isAfter(vehiculo.getFechaFabricacion());
+	}
+	
+	private boolean vehiculoRadicadoEnBsAs(Vehiculo vehiculo) {
+		return vehiculo.ciudadRadicacion().equalsIgnoreCase("Buenos Aires");
+	}
+	
 }
